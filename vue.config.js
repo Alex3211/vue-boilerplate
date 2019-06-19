ca9de16b94e1d6bde57d6de248e8ffd872816b8a
Alex <Alex110397@gmail.com>
2019-06-19 15:06:18 +0200

    fix for edge

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  chainWebpack: (config) => {
+  chainWebpack: config => {
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
     svgRule
+      .oneOf('file-css')
+      .resourceQuery(/file-css/)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: 'assets/[name].[hash:8].[ext]'
+      })
+      .end()
+      .end()
+      .oneOf('external')
       .use('vue-svg-loader')
       .loader('vue-svg-loader')
   }
